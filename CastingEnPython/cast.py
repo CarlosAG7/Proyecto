2ef4a75bdcea39b396f4e0402fc79ebfdebc8c7c
CarlosAG7 <carlos2003ag@hotmail.com>
2022-03-03 22:24:32 -0500

    CastingEnPython

---
+++ CastingEnPython/cast.py
@@ -0,0 +1,9 @@
+#Ejecucion de Cast
+
+numero1 = "20.150"
+
+numero2 = "15.5930"
+
+sum1 = numero1 + (numero2)
+
+print(sum1)
---
+++ CastingEnPython/dictionary.py
@@ -0,0 +1,9 @@
+#Ejecucion de Dictionary
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print("............")
